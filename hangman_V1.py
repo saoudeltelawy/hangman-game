3d3ddf0dd46a32b46969f33fb6578da2c902c403
Saoud ElTelawy <30501682+saoudeltelawy@users.noreply.github.com>
2024-12-03 11:29:13 +0200

    Update hangman_V1.py
    added the code for the first phase [Guessing the letter and display the matched word]

--- hangman_V1.py
+++ hangman_V1.py
@@ -1,1 +1,28 @@
+import random
 
+# List of words for the game
+game_words_list = ["ant", "zee", "camel", "sam", "loe", "zaza"]
+
+# A random word to be chosen!
+game_chosen_word = random.choice(game_words_list)
+print(game_chosen_word)
+
+
+# Create the blanks to represent the word
+blanks = "_ " * len(game_chosen_word)
+print(blanks)
+
+user_guess = input("Guess a letter matching the below blanks: ").lower()
+
+# Create another variable to fill/display the guess letter in the blanks 
+display = ""
+
+for letter in game_chosen_word:
+  if letter == user_guess:
+    print("Right")
+    display+= user_guess + " "
+  else:
+    print("Wrong")
+    display+= "_ "
+
+print(display)
